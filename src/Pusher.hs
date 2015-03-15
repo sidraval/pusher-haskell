8a2cb31d0192b52973d4d0275fd4082bf2b18700
Sid Raval <sid@thoughtbot.com>
2015-03-15 17:30:57 -0400

    Defines generateUrl, requestBody, contentType

--- src/Pusher.hs
+++ src/Pusher.hs
@@ -9,9 +9,14 @@ import Data.Time.Clock.POSIX
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Hash.MD5
 
+type Channel = String
+
 data Pusher = Pusher { pusherAppId :: String
                      , pusherAppKey :: String
                      , pusherAppSecret :: String }
+
+data Event = Event { eventName :: String
+                               , eventData :: String }
 
 authTimestamp :: IO String
 authTimestamp = show <$> round <$> getPOSIXTime
@@ -19,5 +24,17 @@ authTimestamp = show <$> round <$> getPOSIXTime
 baseUrl :: Pusher -> String
 baseUrl (Pusher appId _ _) = "http://api.pusherapp.com.com/apps/" ++ appId
 
-triggerEvent :: Pusher -> Event -> EventData -> IO String
-triggerEvent pusher event eventData = undefined
+triggerEvent :: Pusher -> Channel -> Event -> IO String
+triggerEvent p c e = do
+  url <- generateUrl p c e
+  response <- simpleHTTP $ postRequestWithBody url contentType (requestBody c e)
+  getResponseBody response
+
+generateUrl :: Pusher -> Channel -> Event -> IO String
+generateUrl = undefined
+
+requestBody :: Channel -> Event -> String
+requestBody = undefined
+
+contentType :: String
+contentType = "application/json"
